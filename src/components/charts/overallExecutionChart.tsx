cf8eedbe9f51cabf24092691f9d7bfea16780010
Koushik Chatterjee <koushik350@gmail.com>
2025-08-25 23:58:53 +0530

    🐛 FIX: chart if has 1 values

--- src/components/charts/overallExecutionChart.tsx
+++ src/components/charts/overallExecutionChart.tsx
@@ -87,14 +87,13 @@ export const OverallExecutionResult = memo((props: { summary: Summary }) => {
   ];
   const id = "overallExecutionChart";
   const [activestatus, setActivestatus] = React.useState(() => {
-    const firstAvailable = summaryChartData.find((item) => item?.status);
-    return firstAvailable ? firstAvailable.status : "";
+    const firstAvailable = summaryChartData.find((item) => item.tests > 0);
+    return firstAvailable?.status || "";
   });
 
-  const activeIndex = React.useMemo(
-    () => summaryChartData.findIndex((item) => item.status === activestatus),
-    [activestatus]
-  );
+  const activeIndex = React.useMemo(() => {
+    return summaryChartData.findIndex((item) => item.status === activestatus);
+  }, [activestatus, summaryChartData]);
 
   const availableStatuses = summaryChartData
     .filter((item) => item.tests > 0)
